1283d503e10ad8fc8b041b97da224ca1ae89d888
Piotr Zmudzinski <ptr.zmudzinski@gmail.com>
2016-10-05 12:00:59 +0200

    added post build script

---
+++ UnityProject/SpeechRecognizer/Assets/Editor/KKSetSpeechRecognitionPermissionsOniOS.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEditor.Callbacks;
+using System.Collections;
+using UnityEditor.iOS.Xcode;
+using System.IO;
+
+public class KKSetSpeechRecognitionPermissionsOniOS {
+
+	public static bool shouldRun = true;
+	public static string microphoneUsageDescription = "Pujt something here about microphone usage";
+	public static string speechRecognitionUsageDescription = "Put something here about speech recognition usage";
+
+	private static string nameOfPlist = "Info.plist";
+	private static string keyForMicrophoneUsage = "NSMicrophoneUsageDescription";
+	private static string keyForSpeechRecognitionUsage = "NSSpeechRecognitionUsageDescription";
+
+	[PostProcessBuild]
+	public static void ChangeXcodePlist(BuildTarget buildTarget, string pathToBuiltProject) {
+
+		if (shouldRun && buildTarget == BuildTarget.iOS) {
+
+			// Get plist
+			string plistPath = pathToBuiltProject + "/" + nameOfPlist;
+			PlistDocument plist = new PlistDocument();
+			plist.ReadFromString(File.ReadAllText(plistPath));
+
+			// Get root
+			PlistElementDict rootDict = plist.root;
+
+			rootDict.SetString(keyForMicrophoneUsage,microphoneUsageDescription);
+			rootDict.SetString(keyForSpeechRecognitionUsage, speechRecognitionUsageDescription);
+
+			// Write to file
+			File.WriteAllText(plistPath, plist.WriteToString());
+		}
+	}
+}
