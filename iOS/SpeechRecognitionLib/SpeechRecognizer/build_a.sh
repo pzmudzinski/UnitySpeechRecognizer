1491042e7baf084d57a824d3269a24b0d74a6448
Piotr Zmudzinski <ptr.zmudzinski@gmail.com>
2016-10-05 18:21:54 +0200

    set iOS8 target o 8; embed bit code

--- iOS/SpeechRecognitionLib/SpeechRecognizer/build_a.sh
+++ iOS/SpeechRecognitionLib/SpeechRecognizer/build_a.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-xcodebuild -sdk iphoneos
-xcodebuild -sdk iphonesimulator
+xcodebuild OTHER_CFLAGS="-fembed-bitcode" -sdk iphoneos
+xcodebuild OTHER_CFLAGS="-fembed-bitcode" -sdk iphonesimulator
 lipo -create -output KKSpeechRecognizer.a build/Release-iphoneos/libSpeechRecognizer.a build/Release-iphonesimulator/libSpeechRecognizer.a
